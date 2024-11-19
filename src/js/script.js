3f5d7881009e4af27e865f0135678d30e85da6d2
Maena-Rabusseau <maena.rabusseau@edu.univ-fcomte.fr>
2024-11-19 15:28:43 +0100

    Défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,23 @@
 // *** CAROUSEL ***
 // TODO
-carousel=document.querySelector(".carousel__container")
+const carousel=document.querySelector(".carousel__container");
 carousel.scrollLeft = 520;
+
+const prevButton=document.querySelector('.carousel__button--prev');
+
+ const nextButton= document.querySelector('.carousel__button--next');
+
+
+
+// Largeur de défilement d’un item
+if (carousel) {
+  // Scroll au clic sur le bouton précédent
+  prevButton.addEventListener("click", () => {
+  carousel.scrollLeft -= carousel.offsetWidth;
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.addEventListener("click", () => {
+    carousel.scrollLeft += carousel.offsetWidth ;
+  });
+}
